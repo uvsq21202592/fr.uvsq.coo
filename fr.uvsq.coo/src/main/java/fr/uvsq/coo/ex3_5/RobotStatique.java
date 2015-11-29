6486c0fb96e47c5b8e44021f89876cd3901a75b1
Utilisateur <andredosreis200@gmail.com>
2015-11-29 15:22:08 +0100

    exo 5

--- fr.uvsq.coo/src/main/java/fr/uvsq/coo/ex3_5/RobotStatique.java
+++ fr.uvsq.coo/src/main/java/fr/uvsq/coo/ex3_5/RobotStatique.java
@@ -7,6 +7,9 @@ package fr.uvsq.coo.ex3_5;
  * @author user
  *
  */
-public class RobotStatique {
+public class RobotStatique extends RobotBase {
+	RobotStatique(Position position, Direction direction) {
+		super(position, direction);
+	}
 
 }
